de324c8a06ba3f38ee337ed2bec6eabbd76f7ceb
shehiin <145630739+shehiin@users.noreply.github.com>
2025-04-11 18:41:12 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,7 +3,7 @@ import type { Database } from "@/types/supabase";
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient<Database>(
-  import.meta.env.VITE_SUPABASE_URL || "https://example.supabase.co",
+  import.meta.env.VITE_SUPABASE_URL || "https://btfqinluumsxtswbaeyy.supabase.co",
   import.meta.env.VITE_SUPABASE_ANON_KEY ||
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhtdWZtbXFtYWJpaXdtcnlwZWJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTg2MDM0NzcsImV4cCI6MjAxNDE3OTQ3N30.demo-key",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ0ZnFpbmx1dW1zeHRzd2JhZXl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzNjQ1MjQsImV4cCI6MjA1OTk0MDUyNH0.zz5VkVKPOYoqEHHhOagdJ62xx86BddGSu2VcFCsgJ6M",
 );
